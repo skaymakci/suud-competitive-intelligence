946e7e9b97a22467a80273de4c6f1eedeb7fe9ac
skaymakci <s.kaymakci@suudcollection.com>
2025-12-27 20:51:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print("TEST BASLADI")
-print("HER SEY YOLUNDA")
+from datetime import datetime
+import gspread
+from google.oauth2.service_account import Credentials
+
+print("BASLADI")
+
+scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+creds = Credentials.from_service_account_file("credentials.json", scopes=scopes)
+client = gspread.authorize(creds)
+
+sheet = client.open("SUUD").worksheet("SUUD")
+
+today = datetime.now().strftime("%Y-%m-%d")
+
+sheet.append_row([
+    today,
+    "SISTEM TEST",
+    "OK"
+])
+
+print("SHEETS YAZILDI")
