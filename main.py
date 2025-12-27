12bbfb7a7948e3abd974aa1869ad7f20a2bab730
skaymakci <s.kaymakci@suudcollection.com>
2025-12-27 20:35:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("🚀 Automation is working")
+from datetime import datetime
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Google Sheets bağlantısı
+scopes = ["https://docs.google.com/spreadsheets/d/16ZJmLx86ZZu2PjnCQ-K2SCOGijSNbGnAJoYOp86i8vY/edit?gid=0#gid=0"]
+creds = Credentials.from_service_account_file("credentials.json", scopes=scopes)
+client = gspread.authorize(creds)
+
+sheet = client.open("SUUD").sheet1  # SHEET ADI BURASI
+
+today = datetime.now().strftime("%Y-%m-%d")
+
+sheet.append_row([
+    today,
+    "TEST ÜRÜN",
+    "ZARA",
+    "2999 TL"
+])
